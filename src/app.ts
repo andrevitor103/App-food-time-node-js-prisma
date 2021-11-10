09a332447fb5812d8cf0611d0008cd2af5c20a61
André Vitor <andrevitor103@gmail.com>
2021-11-09 23:25:13 -0300

    Adicição rota companyModule

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { SnacksRouter } from "./routers";
+import { SnacksRouter, CompanyRouter } from "./routers";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(SnacksRouter);
+app.use(SnacksRouter, CompanyRouter);
 
 export { app };
