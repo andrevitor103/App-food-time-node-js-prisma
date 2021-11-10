392fa36f5c83861cff5e187d4ce9484acfac4e8f
André Vitor <andrevitor103@gmail.com>
2021-11-09 22:56:09 -0300

    adição de json e urlenconde na aplicação

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { SnacksRouter } from "./routers";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(SnacksRouter);
 
