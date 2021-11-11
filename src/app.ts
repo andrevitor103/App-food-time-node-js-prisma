a8508acf195cf1fbc75bac12cac1c7892314f9b8
André Vitor <andrevitor103@gmail.com>
2021-11-11 09:35:07 -0300

    Adicao da rota de comentário

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { SnacksRouter, CompanyRouter } from "./routers";
+import { SnacksRouter, CompanyRouter, CommentRouter } from "./routers";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(SnacksRouter, CompanyRouter);
+app.use(SnacksRouter, CompanyRouter, CommentRouter);
 
 export { app };
